57431d431397d04a32c3e52d15f43c9f82705f8b
river0203 <65986354+river0203@users.noreply.github.com>
2023-04-21 11:11:02 +0900

    setting

---
+++ StreetFighter2/Assets/Scripts/UserCon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserCon : MonoBehaviour
+{
+    //시작화면 넘기기 캐릭터 선택과 같은 컨트롤 외 상호작용
+    void Update()
+    {
+        
+    }
+}
